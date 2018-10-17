b868cbb3c97586590be79ab65da64d082fb81b07
truongnn <truongnn@blockchainlabs.asia>
2018-10-17 15:31:40 +0700

    modify server.js

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/index-critical',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 5000)
 
-console.log("Running at Port 3000");
+console.log("Running at Port 5000");
